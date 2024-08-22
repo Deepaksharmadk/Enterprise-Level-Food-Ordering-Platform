cee60c1a1b6b83acc3582dbc594d19fb9a268c06
deepaksharmadk <dks847203@gmail.com>
2024-08-22 14:52:00 +0530

    connect to data base via atlas

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { json } from "stream/consumers";
+import mongoose from "mongoose";
 const app = express();
+mongoose
+  .connect(process.env.MONGODB_URL as string)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 app.use(express.json());
 app.use(cors());
 app.get("/test", (req: express.Request, res: express.Response) => {
