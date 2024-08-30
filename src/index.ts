020c0730a7c2138cdc15dd4ba27e6a277cbaf604
deepaksharmadk <dks847203@gmail.com>
2024-08-31 01:41:14 +0530

    add cors for Security

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,11 @@ mongoose
     console.log(e);
   });
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
 app.get('/helth', (req: express.Request, res: express.Response) => {
   res.json({
     message: 'helth is good',
